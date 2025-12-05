7138f000cd9e4b7902badede5a3ec77efdbe58ce
enricliu <liuenyan0819@gmail.com>
2025-12-05 17:10:22 +0800

    refactor(service): move Service struct to a new file and update its implementation

--- sidecar/src/service/mod.rs
+++ sidecar/src/service/mod.rs
@@ -1,11 +1,11 @@
 mod coached;
-mod addons;
+pub mod addons;
+mod service;
 
 pub use coached::CoachedProcess;
-use coached::CoachedProcessSpawner;
+pub use service::Service;
 
 use log::{info, trace, warn};
-use tokio::sync::watch;
 
 pub const GAME_END_TIMESTEP: u16 = 6000;
 
@@ -46,43 +46,4 @@ pub async fn start_singleton_service() -> Result<(), Box<dyn std::error::Error>>
     }
 
     Ok(())
-}
-
-pub struct Service {
-    process: CoachedProcess,
-    time_rx: watch::Receiver<Option<u16>>,
-}
-
-impl Service {
-    pub async fn new() -> Self {
-        let spawner = CoachedProcess::spawner().await;
-        let process = spawner.spawn().await.unwrap();
-        info!("[Service] Process spawned");
-
-        Self::from_coached_process(process)
-    }
-
-    pub fn from_coached_process(process: CoachedProcess) -> Self {
-        let time_rx = process.coach()
-            .add_caller_addon::<addons::TimeStatusAddon>("time");
-        info!("[Service] Time status addon registered");
-
-        Self {
-            process,
-            time_rx,
-        }
-    }
-
-    pub fn time_watch(&self) -> watch::Receiver<Option<u16>> {
-        self.time_rx.clone()
-    }
-
-    pub fn time(&self) -> Option<u16> {
-        self.time_rx.borrow().clone()
-    }
-
-    pub async fn shutdown(self) -> Result<(), Box<dyn std::error::Error>> {
-        self.process.shutdown().await?;
-        Ok(())
-    }
 }
---
+++ sidecar/src/service/service.rs
@@ -0,0 +1,44 @@
+use log::info;
+use tokio::sync::watch;
+
+use super::addons;
+use super::CoachedProcess;
+
+pub struct Service {
+    process: CoachedProcess,
+    time_rx: watch::Receiver<Option<u16>>,
+}
+
+impl Service {
+    pub async fn new() -> Self {
+        let spawner = CoachedProcess::spawner().await;
+        let process = spawner.spawn().await.unwrap();
+        info!("[Service] Process spawned");
+
+        Self::from_coached_process(process)
+    }
+
+    pub(super) fn from_coached_process(process: CoachedProcess) -> Self {
+        let time_rx = process.coach()
+            .add_caller_addon::<addons::TimeStatusAddon>("time");
+        info!("[Service] Time status addon registered");
+
+        Self {
+            process,
+            time_rx,
+        }
+    }
+
+    pub fn time_watch(&self) -> watch::Receiver<Option<u16>> {
+        self.time_rx.clone()
+    }
+
+    pub fn time(&self) -> Option<u16> {
+        self.time_rx.borrow().clone()
+    }
+
+    pub async fn shutdown(self) -> Result<(), Box<dyn std::error::Error>> {
+        self.process.shutdown().await?;
+        Ok(())
+    }
+}
