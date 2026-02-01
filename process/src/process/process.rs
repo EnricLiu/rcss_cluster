ec8cd89ce0ff9d1833bc73ef57ff68a1fcbd88b2
enricliu <liuenyan0819@gmail.com>
2026-02-01 11:56:03 +0800

    feat(process): integrate overwrite ring buffer for stdout and stderr logging

--- process/src/process/process.rs
+++ process/src/process/process.rs
@@ -4,27 +4,31 @@ use std::process::ExitStatus;
 use std::sync::{Arc, Mutex};
 use std::sync::atomic::AtomicU32;
 use std::time::Duration;
-
 use log::{debug, error, info, trace, warn};
 use nix::sys::signal::{Signal, kill};
 use nix::unistd::Pid;
 use tokio::io::{AsyncBufReadExt, BufReader};
 use tokio::process::Child;
-use tokio::sync::{mpsc, watch};
+use tokio::sync::{mpsc, watch, RwLock};
 use tokio::task::JoinHandle;
+
+use common::ringbuf::OverwriteRB;
 
 use super::builder::ServerProcessSpawner;
 use super::*;
 
 pub const READY_LINE: &str = "Hit CTRL-C to exit";
+pub const STDIO_REPORT_DURATION: Duration = Duration::from_millis(500);
 
 #[derive(Debug)]
 pub struct ServerProcess {
     pid: Arc<AtomicU32>,
     handle: JoinHandle<(io::Result<ExitStatus>, Child)>,
     sig_tx: mpsc::Sender<Signal>,
-    stdout_rx: mpsc::Receiver<String>,
-    stderr_rx: mpsc::Receiver<String>,
+
+    // stdio_report_duration: Duration,
+    stdout: Arc<RwLock<OverwriteRB<String, 32>>>,
+    stderr: Arc<RwLock<OverwriteRB<String, 32>>>,
 
     status_rx: watch::Receiver<Status>,
 }
@@ -37,42 +41,53 @@ impl ServerProcess {
     }
 
     pub(crate) async fn try_from(mut child: Child) -> Result<Self> {
-        let pid = match child.id() {
-            Some(pid) => pid,
-            None => match child.try_wait() {
-                Ok(Some(status)) => return Err(Error::ChildAlreadyCompleted(status)),
-                Ok(None) => return Err(Error::ChildRunningWithoutPid), // todo!("handle it with `child` internally")
-                Err(e) => return Err(Error::ChildUntrackableWithoutPid(e)), // todo!("handle it with `child` internally")
-            },
-        };
+        match child.try_wait() {
+            Ok(None) => {},
+            Ok(Some(status)) => return Err(Error::ChildAlreadyCompleted(status)),
+            Err(e) => return Err(Error::ChildUntrackableWithoutPid(e)), // todo!("handle it with `child` internally")
+        }
+
+        let pid = child.id().ok_or(Error::ChildRunningWithoutPid)?;
 
         let arc_pid = Arc::new(AtomicU32::new(pid));
         let pid = Pid::from_raw(pid as i32);
 
         let (status_tx, status_rx) = watch::channel(Status::Init);
         let (sig_tx, mut sig_rx) = mpsc::channel(4);
-        let (stdout_tx, stdout_rx) = mpsc::channel(32);
-        let (stderr_tx, stderr_rx) = mpsc::channel(32);
+        let stdout_rb = Arc::new(RwLock::new(OverwriteRB::new()));
+        let stderr_rb = Arc::new(RwLock::new(OverwriteRB::new()));
 
         let arc_pid_ = Arc::clone(&arc_pid);
+        let stdout_rb_ = Arc::clone(&stdout_rb);
+        let stderr_rb_ = Arc::clone(&stderr_rb);
         let handle = tokio::spawn(async move {
             let mut child = child;
             let arc_pid = arc_pid_;
-            let stdout = child.stdout.take().ok_or_else(|| {
-                error!("Failed to capture stdout from child process");
-                io::Error::new(io::ErrorKind::Other, "stdout not available")
-            }).expect("stdout should be available with Stdio::piped()");
-            let stderr = child.stderr.take().ok_or_else(|| {
-                error!("Failed to capture stderr from child process");
-                io::Error::new(io::ErrorKind::Other, "stderr not available")
-            }).expect("stderr should be available with Stdio::piped()");
 
-            let mut stdout_reader = BufReader::new(stdout).lines();
-            let mut stderr_reader = BufReader::new(stderr).lines();
+            let mut stdout_reader = {
+                let stdout = child.stdout.take().ok_or_else(|| {
+                    error!("Failed to capture stdout from child process");
+                    io::Error::new(io::ErrorKind::Other, "stdout not available")
+                }).expect("stdout should be available with Stdio::piped()");
+
+                BufReader::new(stdout).lines()
+            };
+
+            let mut stderr_reader = {
+                let stderr = child.stderr.take().ok_or_else(|| {
+                    error!("Failed to capture stderr from child process");
+                    io::Error::new(io::ErrorKind::Other, "stderr not available")
+                }).expect("stderr should be available with Stdio::piped()");
+
+                BufReader::new(stderr).lines()
+            };
 
             if status_tx.send(Status::Booting).is_err() {
                 warn!("Failed to send Booting status: receiver dropped");
             }
+
+            let mut stdout_buf = Vec::with_capacity(32);
+            let mut stderr_buf = Vec::with_capacity(8);
 
             loop {
                 tokio::select! {
@@ -87,7 +102,7 @@ impl ServerProcess {
                             warn!("Failed to send exit status: receiver dropped");
                         }
                         return (status, child);
-                    }
+                    },
 
                     Some(sig) = sig_rx.recv() => {
                         match kill(pid, sig) {
@@ -96,7 +111,7 @@ impl ServerProcess {
                                 error!("Failed to send signal {:?} to child process: {}", sig, e);
                             }
                         }
-                    }
+                    },
 
                     result = stdout_reader.next_line() => {
                         match result {
@@ -107,10 +122,7 @@ impl ServerProcess {
                                         warn!("Failed to send Running status: receiver dropped");
                                     }
                                 }
-                                // if stdout_tx.send(line).await.is_err() {
-                                //     // Channel closed, probably the receiver was dropped.
-                                //     break;
-                                // }
+                                stdout_buf.push(line);
                             }
                             Ok(None) => break, // stdout closed
                             Err(e) => {
@@ -118,21 +130,29 @@ impl ServerProcess {
                                 break;
                             }
                         }
-                    }
+                    },
 
                     result = stderr_reader.next_line() => {
                         match result {
                             Ok(Some(line)) => {
                                 trace!("stderr: {}", line);
-                                // if stderr_tx.send(line).await.is_err() {
-                                //     break;
-                                // }
+                                stderr_buf.push(line);
                             }
                             Ok(None) => break, // stderr closed
                             Err(e) => {
                                 error!("Error reading from stderr: {}", e);
                                 break;
                             }
+                        }
+                    },
+
+                    _ = tokio::time::sleep(STDIO_REPORT_DURATION) => {
+                        if !stdout_buf.is_empty() {
+                            stdout_rb_.write().await.push_many(stdout_buf.drain(..));
+                        }
+
+                        if !stderr_buf.is_empty() {
+                            stderr_rb_.write().await.push_many(stderr_buf.drain(..));
                         }
                     }
                 }
@@ -155,9 +175,17 @@ impl ServerProcess {
             pid: arc_pid,
             sig_tx,
             status_rx,
-            stdout_rx,
-            stderr_rx,
+            stdout: stdout_rb,
+            stderr: stderr_rb,
         })
+    }
+
+    pub async fn stdout_logs(&self) -> Vec<String> {
+        self.stdout.read().await.to_vec()
+    }
+
+    pub async fn stderr_logs(&self) -> Vec<String> {
+        self.stderr.read().await.to_vec()
     }
 
     pub async fn shutdown(&mut self) -> Result<ExitStatus> {
@@ -240,9 +268,7 @@ impl ServerProcess {
 
     /// Wait until the rcssserver is ready and able to accept Udp connections.
     pub async fn until_ready(&mut self, timeout: Option<Duration>) -> Result<()> {
-        if self.try_ready()? {
-            return Ok(());
-        }
+        if self.try_ready()? { return Ok(()) }
 
         let task = self.status_rx.wait_for(|s| s.is_ready());
         let ret = match timeout {
