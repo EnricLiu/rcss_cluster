9c848e22bb3e08aa4707996b319e77a3fa1a3d95
enricliu <liuenyan0819@gmail.com>
2026-02-03 23:33:45 +0800

    refactor(server): reorganize module structure

--- server/src/state.rs
+++ server/src/state.rs
@@ -1,31 +1,22 @@
-mod error;
-mod http;
-mod response;
-mod ws;
-
-use std::pin::Pin;
 use std::sync::{Arc, Weak};
 
-use axum::Router;
-use tokio::net::{TcpListener, ToSocketAddrs};
 use tokio::sync::oneshot;
-use tokio::task::JoinHandle;
 use dashmap::DashMap;
 use chrono::{Utc, Duration};
 use log::{debug, error, info};
 
-pub use error::Error;
-pub use response::Response;
-
-use tower_http::trace::TraceLayer;
 use uuid::Uuid;
-
 use service::Service;
+
+
+pub struct ProxyPlayer {
+    client: common::client::Client,
+}
 
 #[derive(Clone)]
 pub struct AppState {
-    service: Arc<Service>,
-    players: Arc<DashMap<Uuid, Weak<common::client::Client>>>,
+    pub(crate) service: Arc<Service>,
+    pub(crate) players: Arc<DashMap<Uuid, Weak<common::client::Client>>>,
 }
 
 impl AppState {
@@ -79,51 +70,4 @@ impl AppState {
             }
         }
     }
-}
-
-fn route(state: AppState) -> Router {
-    Router::new()
-        .merge(http::route("/", state.clone()))
-        .merge(ws::route("/player", state))
-        .route_layer(TraceLayer::new_for_http())
-}
-
-pub async fn listen(
-    addr: impl ToSocketAddrs,
-    service: Service,
-    shutdown: Option<impl Future<Output=()> + Send + 'static>
-) -> JoinHandle<Result<(), String>> {
-    let (shutdown_tx, shutdown_rx) = oneshot::channel();
-    let state = AppState::new(service, Some(shutdown_rx));
-
-    state.service.spawn().await.expect("FATAL: Service failed to start");
-
-    let app = route(state);
-    let listener = TcpListener::bind(addr).await.unwrap();
-    let addr = listener.local_addr().unwrap();
-    tokio::spawn(async move {
-        let serve = axum::serve(listener, app);
-        info!("Listening on http://{addr}");
-
-        let shutdown: Pin<Box<dyn Future<Output=()> + Send>> = match shutdown {
-            Some(signal) => Box::pin(signal),
-            None => Box::pin(futures::future::pending::<()>()),
-        };
-
-        let signal = async {
-            tokio::select! {
-                _ = shutdown => {
-                    debug!("[Server] Shutdown signal received, shutting down...");
-                },
-                _ = tokio::signal::ctrl_c() => {
-                    debug!("[Server] Ctrl-C received, shutting down...");
-                },
-            }
-
-            let _ = shutdown_tx.send(());
-            debug!("[Server] Shutdown signal sent to AppState cleaner");
-        };
-
-        serve.with_graceful_shutdown(signal).await.map_err(|e| e.to_string())
-    })
 }
